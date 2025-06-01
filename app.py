102e7badccdf411f54bcbc9c06bccbba08408f3d
benoniokaikoi <derhain@gmail.com>
2025-06-01 20:53:09 +0000

    Initial build

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_cors import CORS
 from datetime import datetime
 from dotenv import load_dotenv
@@ -13,10 +14,13 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-# Database configuration
+# Database configuration#
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+# Initialize Flask-Migrate
+migrate = Migrate(app, db)
 
 # Models
 class User(db.Model):
