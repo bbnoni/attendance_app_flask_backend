a9e250f2abe853c2218513eea30eae1aa0ac7306
benoniokaikoi <derhain@gmail.com>
2025-06-01 21:29:38 +0000

    Fix: Updated for Render deployment

--- app.py
+++ app.py
@@ -163,4 +163,4 @@ def get_all_attendance():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
